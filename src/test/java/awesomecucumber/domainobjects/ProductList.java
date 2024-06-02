761f67b1723212e945c8add99ef5cfd8a569d822
sathya <selan05@safeway.com>
2024-06-02 09:41:02 +0530

    initial commit

---
+++ src/test/java/awesomecucumber/domainobjects/ProductList.java
@@ -0,0 +1,20 @@
+package awesomecucumber.domainobjects;
+
+public class ProductList {
+        private Product[] products;
+
+        public void addAllProducts(Product[] products){
+            this.products = products;
+        }
+
+        public Product getProductByName(String name) throws Exception {
+            for(Product product: products){
+                if(product.getName().equals(name)){
+                    return product;
+                }
+            }
+            throw new Exception("Product with name: " + name + " not found");
+        }
+
+
+}
